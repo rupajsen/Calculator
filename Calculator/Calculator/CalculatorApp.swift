86a37cfbb7f1c13b752077c4b3b63e28db31758c
Rupaj Sen <rupajps4@gmail.com>
2023-08-20 12:57:12 +0530

    Add files via upload

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Rupaj Sen on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
